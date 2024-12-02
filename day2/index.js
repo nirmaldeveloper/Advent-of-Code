011579099297cb36637d57e286864b16ff488229
lanka nirmal kumar <55528895+nirmaldeveloper@users.noreply.github.com>
2024-12-02 19:19:48 +0530

    Update index.js

--- day2/index.js
+++ day2/index.js
@@ -18,43 +18,89 @@ async function processLineByLine() {
     lst1.push(output);
   }
 };
+function checkLimit(j){
+  let y = (Math.abs(j) === 3 || Math.abs(j) === 2 || Math.abs(j) === 1);
+  return y;
+}
+
+let count = 0;
+function IsSafeReport(numbers){
+  let i =1;
+  if(numbers.length < 2)
+  {
+    return false;
+  }
+  let incr = numbers[1] > numbers[0];
+  if(incr){
+    for(let x = 0; x < numbers.length-1; x++){
+         let z = numbers[x] - numbers[x+1];
+         if( z < 0 && checkLimit(z)){
+           i++;
+         }
+      else{
+        return false;
+      }
+
+    }
+  }
+  else if(!incr){
+    for(let x = 0; x < numbers.length-1; x++){
+       let z = numbers[x] - numbers[x+1];
+       if( z > 0 && checkLimit(z)){
+            i++;
+       }
+      else{
+        return false;
+      }
+    }
+  }
+  else{
+    return false;
+  }
+  //console.log(numbers);
+  
+  if( i == numbers.length){
+    return true;
+  }
+  
+  return false;
+}
 
 processLineByLine().then(()=>{
-  let count = 0;
-  for(let x =0; x<lst1.length; x++){
-  //console.log(lst1[x]);
-  let lst = lst1[x];
-  let i =1;
-  if(lst[0] > lst[1]){
-    while(i<lst.length){
-      let x = lst[i-1];
-      let y = lst[i];
-      let z = x -y;
-      if((x > y) && !(z < 1 || z > 3)){
-        i++;
-      }
-      else{break;}
+  //Part1
+  let count_safe = 0;
+  for(let x =0; x<lst1.length; x++)
+  {
+  
+  let lst_a = lst1[x];
+  if(IsSafeReport(lst_a)){
+    count_safe++;
+  }
+  }
+  console.log(count_safe);
+  
+  //Part2
+  for(let x =0; x<lst1.length; x++)
+  {
+    let lst = lst1[x];
+    if(IsSafeReport(lst))
+    {
+      count++;
     }
-  }
-  else if(lst[0] < lst[1]){
-    while(i<lst.length){
-      let x = lst[i-1];
-      let y = lst[i];
-      let z = y - x;
-      if((x < y) && !(z < 1 || z > 3)){
-        i++;
+    else
+    {
+      //Since we found the array is not safe, let's find if it    //could be made safe by just excluding one element
+    for(let a = 0; a< lst.length; a++){
+      const chunck = [...lst.slice(0,a), ...lst.slice(a+1)];
+      
+      if(IsSafeReport(chunck))
+      {
+        count++;
+        break; //break after finding which number to exclude
+       }
       }
-      else{break;}
-  }
     }
-  if( i == lst.length){
-    count++;
-  }
   }
   console.log(count);
+  
 });
-
-
-
-
-
