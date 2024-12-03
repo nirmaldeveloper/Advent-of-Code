f0b45c3771d5bd30e604225cbe7bbd8eed42de87
lanka nirmal kumar <55528895+nirmaldeveloper@users.noreply.github.com>
2024-12-03 13:11:19 +0530

    Update main.py

--- day3/main.py
+++ day3/main.py
@@ -1,4 +1,5 @@
 import re
+from typing import Pattern
 
 input = ""
 
@@ -6,10 +7,29 @@ with open("input.csv", "r") as f:
   input = f.read()
 
 f.close()
-sum =0
-pattern_match = r"mul\((\d+)\,(\d+)\)"
-output = re.findall(pattern_match, input)
+#part 1
+total_sum =0
+def findPattern(input):
+  sum = 0
+  pattern_match = r"mul\((\d+)\,(\d+)\)"
+  output = re.findall(pattern_match, input)
+  
+  for i in output:
+    sum += int(i[0]) * int(i[1])
+  return sum
 
-for i in output:
-  sum += int(i[0]) * int(i[1])
-print(sum)
+total_sum += findPattern(input)
+print(total_sum)
+
+
+#part 2
+total_conditional_sum = 0
+d= "don't"
+input_split = input.split(d)
+total_conditional_sum += findPattern(input_split[0])
+for i in range(1, len(input_split)):
+  #This string can be split on do alone as the don't is already split
+  input_do_split = input_split[i].split("do")
+  for j in range(1, len(input_do_split)):
+    total_conditional_sum += findPattern(input_do_split[j])
+print(total_conditional_sum)
